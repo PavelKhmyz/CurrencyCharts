289e6ec620422cb36396c092dfa63a77699be222
Pavel <pavlik.schavlik@outlook.com>
2022-10-24 23:35:47 +0300

    hw8_firstWorkingBuid

--- api.js
+++ api.js
@@ -1,3 +1,18 @@
+function getUserData(){
+    const userData = {};
+    userData.currencyName = document.querySelector('.currencyName').value;
+    userData.dateFrom = document.querySelector('.dateFrom').value;
+    userData.dateTo = document.querySelector('.dateTo').value;
+    console.log(userData);
+    search(userData);
+}
+
+async function sendRequest(url){
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
+
 async function search(data){
     const bla = await sendRequest(url);
     const curArr = [];
@@ -12,57 +27,134 @@ async function search(data){
             curArr.push(element)
         }
     })
-    console.log(curArr);
-    getRate(curArr, data)
-    
-    // const dinamicUrl = `https://www.nbrb.by/api/exrates/rates/dynamics/${lol.Cur_ID}?startDate=${data.dateTo}&endDate=${data.dateFrom}`;
-    
-    // const getDinamics = await sendRequest(dinamicUrl);
-    // console.log(getDinamics)
+    if(!data.dateTo){
+        getCur(curArr, data);
+    }
+    else if(new Date(data.dateFrom) - new Date(data.dateTo) < year){
+        const changeDate = new Date(new Date(data.dateFrom) - year);
+        data.dateTo = `${changeDate.getFullYear()}-${changeDate.getMonth() + 1}-${changeDate.getDate()}`;
+        getRate(curArr, data);
+    }
+    else{
+        getRate(curArr, data);
+    }
+    console.log(curArr, data);
 };
 
-async function getRate(curArr, data){
-    let startDate = data.dateTo; 
-    const bla = new Date(data.dateTo);
-    bla.setFullYear(bla.getFullYear() + 1);
-    let endDate = `${bla.getFullYear()}-${bla.getMonth()}-${bla.getDate()}`
-    
-       
+async function getCur(curArr, data){
     let curId;
-    
+    const rateArr = [];
+    for(let i = 0; i < curArr.length; i++){
+        if (+ new Date(data.dateFrom) > + new Date(curArr[i].Cur_DateEnd)){
+
+        }
+        else {
+            curId = curArr[i].Cur_ID;
+            break;
+        };
+    };
+    const createUrl = `https://www.nbrb.by/api/exrates/rates/${curId}?ondate=${data.dateFrom}`;
+    const request = await sendRequest(createUrl);
+    rateArr.push(request);
+    console.log(rateArr);
+}
+
+async function getRate(curArr, data){
+    const startDate = data.dateTo;
+    const endDate = data.dateFrom;
+    let requestCounter = Math.floor(((new Date(endDate) - new Date(startDate))) / year);
+    const rateArr = [];
+
+    let start = startDate;
+
+    const parseDate = new Date(start);
+    parseDate.setFullYear(parseDate.getFullYear() + 1);
+
+    let end =  `${parseDate.getFullYear()}-${parseDate.getMonth() + 1}-${parseDate.getDate()}`;
+    let curId;
 
     for(let i = 0; i < curArr.length; i++){
-        if(new Date(startDate) >= new Date(curArr[i].Cur_DateStart)){
+        if (+ new Date(start) > + new Date(curArr[i].Cur_DateEnd)){
+
+        }
+        else {
             curId = curArr[i].Cur_ID;
+            break;
         };
     };
 
-    let dinamicUrl = `https://www.nbrb.by/api/exrates/rates/dynamics/${curId}?startDate=${startDate}&endDate=${endDate}`;
-    
-    // const requestCounter = Math.floor((new Date(data.dateFrom) - new Date(data.dateTo)) / year);
-    // const rateArr = [];
-    // for(let i = 0; i < requestCounter; i++){
-    //     const request = await sendRequest(dinamicUrl)
-    //     rateArr.push(request);
+    for(let i = 0; i < requestCounter; i++){
+        for(let i = 0; i < curArr.length; i++){
+            if(+ new Date(start) > + new Date(curArr[i].Cur_DateEnd)){
 
-    //     const changeEndDate = new Date(endDate);
-    //     changeEndDate.setFullYear(changeEndDate.getFullYear + 1);
-    //     const changeStartDate = new Date(startDate);
-    //     changeStartDate.setFullYear(changeStartDate.getFullYear + 1);
+            }
+            else if(+ new Date(start) >= + new Date(curArr[i].Cur_DateStart) && + new Date(end) <= + new Date(curArr[i].Cur_DateEnd)){
+                    curId = curArr[i].Cur_ID;
 
-    //     endDate = `${changeEndDate.getFullYear()}-${changeEndDate.getMonth()}-${changeEndDate.getDate()}`;
-    //     startDate = `${changeStartDate.getFullYear()}-${changeStartDate.getMonth()}-${changeStartDate.getDate()}`;
+                    const makeUrl = createUrl(curId, start, end);
+                    const request = await sendRequest(makeUrl);
+                    rateArr.push(request);
+                    // rateArr = request;
 
-    //     for(let i = 0; i < curArr.length; i++){
-    //         if(new Date(startDate) >= new Date(curArr[i].Cur_DateStart) && new Date(endDate) <= new Date(curArr[i].Cur_DateEnd)){
-    //             curId = curArr[i].Cur_ID;
-    //         }
-    //         // else if(new Date(startDate) >= new Date(curArr[i].Cur_DateStart)){
-    //         //     curId = curArr[i].Cur_ID;
-    //         //     startDate = 
-    //         // }
-    //         console.log(startDate, endDate, changeEndDate, changeStartDate)
-    //     };
-    // }
-    console.log(dinamicUrl);
+                    start = end;
+
+                    const lol = new Date(start);
+                    lol.setFullYear(lol.getFullYear() + 1);
+                    end = `${lol.getFullYear()}-${lol.getMonth() + 1}-${lol.getDate()}`;
+
+                    break;
+            }
+            else if(+ new Date(start) >= + new Date(curArr[i].Cur_DateStart) && + new Date(end) > + new Date(curArr[i].Cur_DateEnd)){
+                curId = curArr[i].Cur_ID;                   
+                end = curArr[i].Cur_DateEnd;
+
+                const makeUrl = createUrl(curId, start, end);
+                const request = await sendRequest(makeUrl);
+                rateArr.push(request);
+                // rateArr = request;
+
+                requestCounter += 1;
+                    
+                const lol = new Date(end);
+                lol.setDate(lol.getDate() + 1);
+                start = `${lol.getFullYear()}-${lol.getMonth() + 1}-${lol.getDate()}`;
+
+                const bla = new Date(start);
+                bla.setFullYear(bla.getFullYear() + 1);
+                end = `${bla.getFullYear()}-${bla.getMonth() + 1}-${bla.getDate()}`;
+                    
+                break;
+            };
+        };
+    };
+    // console.log(rateArr);
+    parseForChart(rateArr)
+};
+
+let chartDate = [];
+let chartRate = [];
+
+let bla;
+
+function parseForChart(rateArr){
+    chartDate.length = 0;
+    chartRate.length = 0;
+    rateArr.forEach(element => {
+        for(let i = 0; i < element.length; i++){
+            // console.log(element[i].Date);
+            chartDate.push(element[i].Date);
+            chartRate.push(element[i].Cur_OfficialRate);
+        }
+    })
+    if(bla){
+        bla.destroy()
+    }
+    bla = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
 }
+
+function createUrl(curId, start, end) {
+    return `https://www.nbrb.by/api/exrates/rates/dynamics/${curId}?startDate=${start}&endDate=${end}`;
+};
--- chart.js
+++ chart.js
@@ -1,87 +1,124 @@
-// Copyright 2021 Observable, Inc.
-// Released under the ISC license.
-// https://observablehq.com/@d3/line-chart
-function LineChart(data, {
-    x = ([x]) => x, // given d in data, returns the (temporal) x-value
-    y = ([, y]) => y, // given d in data, returns the (quantitative) y-value
-    defined, // for gaps in data
-    curve = d3.curveLinear, // method of interpolation between points
-    marginTop = 20, // top margin, in pixels
-    marginRight = 30, // right margin, in pixels
-    marginBottom = 30, // bottom margin, in pixels
-    marginLeft = 40, // left margin, in pixels
-    width = 640, // outer width, in pixels
-    height = 400, // outer height, in pixels
-    xType = d3.scaleUtc, // the x-scale type
-    xDomain, // [xmin, xmax]
-    xRange = [marginLeft, width - marginRight], // [left, right]
-    yType = d3.scaleLinear, // the y-scale type
-    yDomain, // [ymin, ymax]
-    yRange = [height - marginBottom, marginTop], // [bottom, top]
-    yFormat, // a format specifier string for the y-axis
-    yLabel, // a label for the y-axis
-    color = "currentColor", // stroke color of line
-    strokeLinecap = "round", // stroke line cap of the line
-    strokeLinejoin = "round", // stroke line join of the line
-    strokeWidth = 1.5, // stroke width of line, in pixels
-    strokeOpacity = 1, // stroke opacity of line
-  } = {}) {
-    // Compute values.
-    const X = d3.map(data, x);
-    const Y = d3.map(data, y);
-    const I = d3.range(X.length);
-    if (defined === undefined) defined = (d, i) => !isNaN(X[i]) && !isNaN(Y[i]);
-    const D = d3.map(data, defined);
+const actions = [
+    {
+      name: 'pointStyle: circle (default)',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'cirlce';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: cross',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'cross';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: crossRot',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'crossRot';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: dash',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'dash';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: line',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'line';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: rect',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'rect';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: rectRounded',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'rectRounded';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: rectRot',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'rectRot';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: star',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'star';
+        });
+        chart.update();
+      }
+    },
+    {
+      name: 'pointStyle: triangle',
+      handler: (chart) => {
+        chart.data.datasets.forEach(dataset => {
+          dataset.pointStyle = 'triangle';
+        });
+        chart.update();
+      }
+    }
+];
+
+const labels = chartDate;
+const rate = chartRate;
   
-    // Compute default domains.
-    if (xDomain === undefined) xDomain = d3.extent(X);
-    if (yDomain === undefined) yDomain = [0, d3.max(Y)];
-  
-    // Construct scales and axes.
-    const xScale = xType(xDomain, xRange);
-    const yScale = yType(yDomain, yRange);
-    const xAxis = d3.axisBottom(xScale).ticks(width / 80).tickSizeOuter(0);
-    const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
-  
-    // Construct a line generator.
-    const line = d3.line()
-        .defined(i => D[i])
-        .curve(curve)
-        .x(i => xScale(X[i]))
-        .y(i => yScale(Y[i]));
-  
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .attr("viewBox", [0, 0, width, height])
-        .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-  
-    svg.append("g")
-        .attr("transform", `translate(0,${height - marginBottom})`)
-        .call(xAxis);
-  
-    svg.append("g")
-        .attr("transform", `translate(${marginLeft},0)`)
-        .call(yAxis)
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".tick line").clone()
-            .attr("x2", width - marginLeft - marginRight)
-            .attr("stroke-opacity", 0.1))
-        .call(g => g.append("text")
-            .attr("x", -marginLeft)
-            .attr("y", 10)
-            .attr("fill", "currentColor")
-            .attr("text-anchor", "start")
-            .text(yLabel));
-  
-    svg.append("path")
-        .attr("fill", "none")
-        .attr("stroke", color)
-        .attr("stroke-width", strokeWidth)
-        .attr("stroke-linecap", strokeLinecap)
-        .attr("stroke-linejoin", strokeLinejoin)
-        .attr("stroke-opacity", strokeOpacity)
-        .attr("d", line(I));
-  
-    return svg.node();
-  }
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: 'Dataset',
+            data: rate,
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(255, 99, 132)',
+            pointStyle: 'circle',
+            pointRadius: 2,
+            pointHoverRadius: 5
+        }
+    ]
+};
+          
+const config = {
+    type: 'line',
+    data: data,
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: (ctx) => 'Point Style: ' + ctx.chart.data.datasets[0].pointStyle,
+            }
+        }
+    }
+};
